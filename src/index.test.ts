c545ec3c8d27c60876318305f8fb1dd815ca4723
Daniel Nixon <dan.nixon@gmail.com>
2019-09-29 18:15:44 +1000

    Update index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -22,24 +22,24 @@ describe("get", () => {
     expect<number | undefined>(get(zs, 1)).toBe(undefined);
     expect<number | undefined>(get(zs, 100)).toBe(undefined);
 
-    // // readonly array
+    // readonly array
     const as = [1, 2, 3] as ReadonlyArray<1 | 2 | 3>;
     expect<1 | 2 | 3 | undefined>(get(as, 1)).toBe(2);
     expect<1 | 2 | 3 | undefined>(get(as, 100)).toBe(undefined);
 
-    // // record
+    // record
     const record = { 1: "asdf" } as Record<number, string>;
     expect<string | undefined>(get(record, 1)).toBe("asdf");
     expect<string | undefined>(get(record, 100)).toBe(undefined);
 
-    // // object
+    // object
     const obj = { 1: "asdf" };
     expect<string>(get(obj, 1)).toBe("asdf");
-    // // const obj100 = get(obj, 100); // doesn't compile
+    // const obj100 = get(obj, 100); // doesn't compile
 
-    // // const object
+    // const object
     const constObj = { 1: "asdf" } as const;
     expect<string>(get(constObj, 1)).toBe("asdf");
-    // // const constObj100 = get(constObj, 100); // doesn't compile
+    // const constObj100 = get(constObj, 100); // doesn't compile
   });
 });
